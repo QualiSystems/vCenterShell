8a75c92085aa77d7bbf57f1de2a80852032c58ef
Boris Modylevsky <borismod@gmail.com>
2015-12-09 17:50:40 +0200

    dvSwitchDataRetriever.py was added

--- vCenterShell/vCenterShell/dvSwitchDataRetriever.py
+++ vCenterShell/vCenterShell/dvSwitchDataRetriever.py
@@ -1,7 +1,19 @@
 class dvSwitchDataRetriever:
 
     def Retrieve_dvSwitchData(host, user, pwd, port=443):
-        vCenterConnection = SmartConnect(host=host, user=user,pwd=pwd,port=port)
+        vCenterConnection = SmartConnect(host=host, user=user, pwd=pwd, port=port)
         content = vCenterConnection.RetrieveContent()
+        host = content.searchIndex.FindByDnsName(dnsName = vmhost, vmSearch = False)
+        result = []
+        for vs in host.config.network.vswitch:
+            if vs.name == name:
+                result.append({
+                    "name" : vs.name,
+                    "numOfPorts" : vs.numPorts,
+                    "numPortsAvailable" : vs.numPortsAvailable,
+                    "mtu" : vs.mtu,
+                    "key" : vs.key
+                })
+        return json.dumps(result)
 
 
