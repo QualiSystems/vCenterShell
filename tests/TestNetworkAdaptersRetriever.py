9dc4360baedda5691ff1a4d403d8486c6b2a621f
Boris Modylevsky <borismod@gmail.com>
2015-12-14 14:32:01 +0200

    test_NetworkAdaptersRetriever was renamed to TestNetworkAdaptersRetriever
    TestNetworkAdaptersRetriever was cleaned up

--- tests/TestNetworkAdaptersRetriever.py
+++ tests/TestNetworkAdaptersRetriever.py
@@ -1,24 +1,17 @@
-﻿import unittest
-from mock import Mock, MagicMock, create_autospec, mock_open, patch
+﻿import os.path
+import sys
+import unittest
+
+from mock import Mock, create_autospec
 from pyVmomi import vim
 
-import sys
-import os.path
+
 sys.path.append(os.path.join(os.path.dirname(__file__), '../vCenterShell/vCenterShell'))
-from pycommon.cloudshellDataRetrieverService import *
 from models.VCenterConnectionDetails import *
 from commands.NetworkAdaptersRetriever import *
-from models.VirtualNic import VirtualNic
 
-class test_NetworkAdaptersRetriever(unittest.TestCase):
 
-    # Integration test, Ignored it on CI
-    def integrationTest_RetrieveDvSwitchData_integrationTest(self):
-        cred = TestCredentials()
-        switchRetriever = NetworkAdaptersRetriever(pyVmomiService(SmartConnect, Disconnect))
-        switch = switchRetriever.Execute()
-
-        self.assertEqual(True, True)
+class TestNetworkAdaptersRetriever(unittest.TestCase):
 
     def test_RetrieveDvSwitchData_unitTest(self):
         content = Mock()
@@ -28,7 +21,7 @@ class test_NetworkAdaptersRetriever(unittest.TestCase):
         vNic = create_autospec(spec=vim.vm.device.VirtualEthernetCard)
         vNic.deviceInfo = Mock()
         vNic.deviceInfo.summary = 'network1'
-        vNic.macAddress = 'AA-BB' 
+        vNic.macAddress = 'AA-BB'
         vNic.connectable = Mock()
         vNic.connectable.connected = 'True'
         vNic.connectable.startConnected = 'True'
@@ -41,7 +34,8 @@ class test_NetworkAdaptersRetriever(unittest.TestCase):
         pvService.get_obj = Mock(return_value=vmMachine)
 
         csRetrieverService = Mock()
-        csRetrieverService.getVCenterInventoryPathAttributeData = Mock(return_value={'vCenter_resource_name': 'Resource Name'})
+        csRetrieverService.getVCenterInventoryPathAttributeData = Mock(
+            return_value={'vCenter_resource_name': 'Resource Name'})
 
         helpers.get_resource_context_details = Mock(return_value={})
 
@@ -61,6 +55,7 @@ class test_NetworkAdaptersRetriever(unittest.TestCase):
         self.assertEqual(nics[0].macAddress, 'AA-BB')
         self.assertEqual(nics[0].connected, 'True')
         self.assertEqual(nics[0].connectAtPowerOn, 'True')
+
 
 if __name__ == '__main__':
     unittest.main()
