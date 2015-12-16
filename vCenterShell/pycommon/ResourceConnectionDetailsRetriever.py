a02eb6b5c915d3663149f95aa67394aec796fbca
Boris Modylevsky <borismod@gmail.com>
2015-12-16 11:03:04 +0200

    cleaning up the code

--- vCenterShell/pycommon/ResourceConnectionDetailsRetriever.py
+++ vCenterShell/pycommon/ResourceConnectionDetailsRetriever.py
@@ -1,23 +1,26 @@
 import sys
 import os.path
 from vCenterShell.models.VCenterConnectionDetails import VCenterConnectionDetails
+import qualipy.scripts.cloudshell_scripts_helpers as helpers
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '../vCenterShell/vCenterShell'))
 
 
 class ResourceConnectionDetailsRetriever:
-
     def __init__(self, cs_retriever_service):
         self.csRetrieverService = cs_retriever_service
 
-    def get_connection_details(self, resource_name):
-        """
+    def connection_details(self, resource_name):
+        """ Retrieves connection details to vCenter from specified resource
+
+        :param resource_name: Resource name to get connection details from
         :rtype VCenterConnectionDetails:
         """
         session = helpers.get_api_session()
-        vCenter_details = session.GetResourceDetails(resource_name)
+        resource_details = session.GetResourceDetails(resource_name)
 
         # get vCenter connection details from vCenter resource
-        vCenterConn = self.csRetrieverService.getVCenterConnectionDetails(session, vCenter_details)
+        connection_details = self.csRetrieverService.getVCenterConnectionDetails(session, resource_details)
 
-        return VCenterConnectionDetails(vCenterConn["vCenter_url"], vCenterConn["user"], vCenterConn["password"])
+        return VCenterConnectionDetails(connection_details["vCenter_url"], connection_details["user"],
+                                        connection_details["password"])
