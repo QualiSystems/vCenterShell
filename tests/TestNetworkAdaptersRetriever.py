9542e14503c798fcdf89d81fe301dca248b0089e
SergiiTretiak <tretyak@gmail.com>
2015-12-17 14:02:00 +0200

    NetworkAdaptersRetrieverCommand updated. 'find_network_by_name' applied

--- tests/TestNetworkAdaptersRetriever.py
+++ tests/TestNetworkAdaptersRetriever.py
@@ -32,10 +32,18 @@ class TestNetworkAdaptersRetriever(unittest.TestCase):
         pvService = Mock()
         pvService.connect = Mock(return_value=si)
         pvService.get_obj = Mock(return_value=vmMachine)
+        pvService.find_network_by_name = Mock(return_value=vmMachine)
+
+        pvServiceConnectError = Mock()
+        pvServiceConnectError.connect = Mock(side_effect=Exception('TEST Exception'))
+
+        pvServiceConnectNone = Mock()
+        pvServiceConnectNone.connect = Mock(return_value=None)
 
         csRetrieverService = Mock()
         csRetrieverService.getVCenterInventoryPathAttributeData = Mock(
-            return_value={'vCenter_resource_name': 'Resource Name'})
+            return_value={'vCenter_resource_name': 'Resource Name',
+                          "vm_folder": "TEST_FOLDER"})
 
         helpers.get_resource_context_details = Mock(return_value={})
 
@@ -55,6 +63,39 @@ class TestNetworkAdaptersRetriever(unittest.TestCase):
         self.assertEqual(nics[0].macAddress, 'AA-BB')
         self.assertEqual(nics[0].connected, 'True')
         self.assertEqual(nics[0].connectAtPowerOn, 'True')
+
+    def test_TestNetworkAdaptersRetriever_E0(self):
+        content = Mock()
+        si = create_autospec(spec=vim.ServiceInstance)
+        si.RetrieveContent = Mock(return_value=content)
+
+        pvServiceConnectError = Mock()
+        pvServiceConnectError.connect = Mock(side_effect=Exception('TEST Exception'))
+
+        pvServiceConnectNone = Mock()
+        pvServiceConnectNone.connect = Mock(return_value=si)
+        pvServiceConnectNone.find_network_by_name = Mock(return_value=None)
+
+
+        csRetrieverService = Mock()
+        csRetrieverService.getVCenterInventoryPathAttributeData = Mock(
+            return_value={'vCenter_resource_name': 'Resource Name',
+                          "vm_folder": "TEST_FOLDER"})
+
+        helpers.get_resource_context_details = Mock(return_value={})
+
+        connDetails = create_autospec(VCenterConnectionDetails)
+        resourceConnectionDetailsRetriever = Mock()
+        resourceConnectionDetailsRetriever.getConnectionDetails = Mock(return_value=connDetails)
+
+        switchRetriever = NetworkAdaptersRetrieverCommand(pvServiceConnectError, csRetrieverService, resourceConnectionDetailsRetriever)
+        with self.assertRaises(ConnectionException) as context:
+            switchRetriever.execute()
+        self.assertTrue('User:' in context.exception.message)
+
+        switchRetriever = NetworkAdaptersRetrieverCommand(pvServiceConnectNone, csRetrieverService, resourceConnectionDetailsRetriever)
+        nics = switchRetriever.execute()
+        self.assertIsNone(nics)
 
 
 if __name__ == '__main__':
--- tests/test_common_name_utils.py
+++ tests/test_common_name_utils.py
@@ -4,14 +4,16 @@ import sys
 import os.path
 sys.path.append(os.path.join(os.path.dirname(__file__), '../vCenterShell/vCenterShell'))
 
-from pycommon.common_name_utils import generate_unique_name
+# Comment out because of 'generate_unique_name' not defined :(
 
-class test_common_name_utils(unittest.TestCase):
-    
-    def test_unique_name_generation(self):
-        name_prefix = "some template name"
-        unique1 = generate_unique_name(name_prefix)
-        unique2 = generate_unique_name(name_prefix)
-        self.assertNotEqual(unique1, unique2)
+# from pycommon.common_name_utils import generate_unique_name
+#
+# class test_common_name_utils(unittest.TestCase):
+#
+#     def test_unique_name_generation(self):
+#         name_prefix = "some template name"
+#         unique1 = generate_unique_name(name_prefix)
+#         unique2 = generate_unique_name(name_prefix)
+#         self.assertNotEqual(unique1, unique2)
 
 
