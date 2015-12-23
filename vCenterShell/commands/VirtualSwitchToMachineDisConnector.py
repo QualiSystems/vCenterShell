05b74b593e6d8058e111af5a5042d9a77cde80e8
Sergii Tretiak <tretyak@gmail.com>
2015-12-23 03:51:34 -0800

    Add disconnector

---
+++ vCenterShell/commands/VirtualSwitchToMachineDisConnector.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+@see https://waffle.io/QualiSystems/vCenterShell/cards/5666b2aa0c076d2300052216 for initial info
+"""
+
+from pyVmomi import vim
+from vCenterShell.pycommon.pyVmomiService import *
+from vCenterShell.pycommon.SynchronousTaskWaiter import SynchronousTaskWaiter
+from vCenterShell.commands.VirtualMachinePortGroupConfigurer import *
+
+
+import qualipy.scripts.cloudshell_scripts_helpers as helpers
+from pyVmomi import vim
+
+from vCenterShell.commands.BaseCommand import BaseCommand
+
+from vCenterShell.pycommon.logger import getLogger
+
+_logger = getLogger("vCenterShell")
+
+
+class VirtualSwitchToMachineDisConnector(BaseCommand):
+    def __init__(self, pyvmomi_service, resource_connection_details_retriever, dv_port_group_creator,
+                 virtual_machine_port_group_configurer):
+        self.pyvmomi_service = pyvmomi_service
+        self.resourceConnectionDetailsRetriever = resource_connection_details_retriever
+        self.dv_port_group_creator = dv_port_group_creator
+        self.virtual_machine_port_group_configurer = virtual_machine_port_group_configurer
+
+    def disconnect(self, virtual_machine_name, dv_switch_path, dv_switch_name, dv_port_name, virtual_machine_path, vm_uuid,
+                port_group_path):
+
+        connection_details = self.resourceConnectionDetailsRetriever.connection_details(virtual_machine_name)
+
+        si = self.pyvmomi_service.connect(connection_details.host, connection_details.username,
+                                          connection_details.password,
+                                          connection_details.port)
+
+        self.dv_port_group_creator.create_dv_port_group(dv_port_name, dv_switch_name, dv_switch_path, si)
+
+        self.virtual_machine_port_group_configurer.configure_port_group_on_vm(si, virtual_machine_path, vm_uuid,
+                                                                              port_group_path,
+                                                                              dv_port_name)
+
+
+     def execute(self):
+         pass
