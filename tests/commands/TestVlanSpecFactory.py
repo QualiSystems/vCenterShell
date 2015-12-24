3f90aeca5fd22f279c45f7bd772896d4b99765c2
Boris Modylevsky <borismod@gmail.com>
2015-12-24 14:36:58 +0200

    vLanServiceDriver was added
    VlanSpecFactory was improved to support Access and Trunk

--- tests/commands/TestVlanSpecFactory.py
+++ tests/commands/TestVlanSpecFactory.py
@@ -6,5 +6,5 @@ class TestVlanSpecFactory(TestCase):
 
     def test_get_vlan_spec(self):
         vlan_spec_factory = VlanSpecFactory()
-        vlan_spec = vlan_spec_factory.get_vlan_spec('VLAN')
+        vlan_spec = vlan_spec_factory.get_vlan_spec('Access')
         self.assertIsNotNone(vlan_spec)
---
+++ vLanService/vLanServiceDriver.py
@@ -0,0 +1,16 @@
+import qualipy.scripts.cloudshell_scripts_helpers as helpers
+from qualipy.api.cloudshell_api import InputNameValue
+
+session = helpers.get_api_session()
+reservation_id = helpers.get_reservation_context_details().id
+connectors = session.GetReservationDetails(reservation_id).ReservationDescription.Connectors
+attributes = helpers.get_resource_context_details().attributes
+vlan = attributes['VLAN Id']
+access_mode = attributes['Access Mode']
+for connector in connectors:
+    if connector.Source == helpers.get_resource_context_details().name:
+        session.ExecuteCommand(reservation_id, connector.Target, 'Resource', 'Connect',
+                               [InputNameValue('COMMAND', "connect"),
+                                InputNameValue('VLAN_ID', vlan),
+                                InputNameValue('VLAN_SPEC_TYPE', access_mode)],
+                               True)
