f5639c28c754a4418c486abef4d08becd3b0ca76
Sergii Tretiak <tretyak@gmail.com>
2015-12-25 09:02:07 -0800

    test demo driver added

--- vCenterShell/demo/dev_bootstrapper.py
+++ vCenterShell/demo/dev_bootstrapper.py
@@ -16,9 +16,13 @@ import qualipy.scripts.cloudshell_scripts_helpers as helpers
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '../vCenterShell/vCenterShell'))
 
+from pycommon.logger import getLogger
+_logger = getLogger(__name__)
+
 def getConnectDataContext():
     global ConnectData
     return ConnectData
+
 
 def update_environment():
     os.environ["RESOURCECONTEXT"] = \
@@ -37,15 +41,34 @@ def update_environment():
 
 class DevBootstrapper(object):
     def __init__(self):
+        update_environment()
+
         self.py_vmomi_service = pyVmomiService(SmartConnect, Disconnect)
         self.data_retriever_service = CloudshellDataRetrieverService()
         self.connection_details_retriever = ResourceConnectionDetailsRetriever(self.data_retriever_service)
+        self.si = None
+
+    def connect(self):
+        data = getConnectDataContext()
+        self.si = self.py_vmomi_service.connect(data["host"],
+                                                data["user"],
+                                                data["password"],
+                                                data["port"])
+
+        _logger.info("CONNECT {}".format("SUCCESS" if self.si else "ERROR"))
+
+    def disconnect(self):
+        if self.si:
+            self.py_vmomi_service.disconnect(self.si)
+            _logger.info("DISCONNECTED")
+        else:
+            _logger.info("WAS NOT CONNECTED")
 
 
-def attachAndGetResourceContext():
-    update_environment()
-    global ConnectData
-    dev_helpers.attach_to_cloudshell_as(ConnectData["user"],
-                                        ConnectData["password"],
-                                        ConnectData["domain"],
-                                        ConnectData["reservationId"])
+# def attachAndGetResourceContext():
+#     update_environment()
+#     global ConnectData
+#     dev_helpers.attach_to_cloudshell_as(ConnectData["user"],
+#                                         ConnectData["password"],
+#                                         ConnectData["domain"],
+#                                         ConnectData["reservationId"])
--- vCenterShell/demo/infrastructure_view.py
+++ vCenterShell/demo/infrastructure_view.py
@@ -4,11 +4,10 @@ vCenter Shell Starter
 
 import os, sys
 import qualipy.scripts.cloudshell_dev_helpers as dev_helpers
-
 from settings import *
 
 from vCenterShell.demo.dev_bootstrapper import DevBootstrapper
-from vCenterShell.demo.dev_bootstrapper import attachAndGetResourceContext
+#from vCenterShell.demo.dev_bootstrapper import attachAndGetResourceContext
 from pycommon.logging_service import LoggingService
 
 
@@ -21,14 +20,38 @@ from vCenterShell.commands.NetworkAdaptersRetrieverCommand import NetworkAdapter
 def main():
     LoggingService(INITIAL_LOG_LEVEL, INITIAL_LOG_LEVEL, DEFAULT_LOG_FILENAME)
     bootstrapper = DevBootstrapper()
+    bootstrapper.connect()
+    bootstrapper.disconnect()
 
-    attachAndGetResourceContext()
+    si = bootstrapper.si
+    vcenter = bootstrapper.py_vmomi_service
 
-    command = NetworkAdaptersRetrieverCommand(bootstrapper.py_vmomi_service,
-                                              bootstrapper.data_retriever_service,
-                                              bootstrapper.connection_details_retriever)
+    #boris = vcenter.find_host_by_name(si, "/QualiSB/a/", 'boris1')
+    #boris = vcenter.find_host_by_name(si, "/", 'boris1')
+    #boris = vcenter.find_vm_by_name(si, "/", 'boris1')
+    #boris = vcenter.find_datacenter_by_name(si, "/", 'QualiSB')
 
-    command.execute()
+    kinds = [   vcenter.ChildEntity,
+                vcenter.VM,
+                vcenter.Network,
+                vcenter.Datacenter,
+                vcenter.Host,
+                vcenter.Datastore,
+                vcenter.Cluster
+    ]
+
+    for k in kinds:
+        a = vcenter.find_item_in_path_by_type(si, "/d1", k)
+        print "{:>20} {}".format(k, a)
+
+
+    # #attachAndGetResourceContext()
+    #
+    # command = NetworkAdaptersRetrieverCommand(bootstrapper.py_vmomi_service,
+    #                                           bootstrapper.data_retriever_service,
+    #                                           bootstrapper.connection_details_retriever)
+    # #
+    # command.execute()
 
 if __name__ == "__main__":
     main()
