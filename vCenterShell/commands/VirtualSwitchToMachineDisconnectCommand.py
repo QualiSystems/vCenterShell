7d9c8cb07a7287a5fb713e7817781f2a91b8e6f1
Raz Abadi <razaba@gmail.com>
2015-12-28 14:01:44 +0200

    Revert "Merge branch 'develop' of https://github.com/QualiSystems/vCenterShell into feature/dv_switch_disconnect3"
    This reverts commit 6580f95bdae8b0203f77bb2ba84781e94bfcd343, reversing
    changes made to 3626b38839366e70b3701998ba1ed7a1459e1b85.
    
    # Conflicts:
    #	pycommon/pyVmomiService.py

---
+++ vCenterShell/commands/VirtualSwitchToMachineDisconnectCommand.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+"""
+@see https://waffle.io/QualiSystems/vCenterShell/cards/5666b2aa0c076d2300052216 for initial info
+
+@see https://www.vmware.com/support/developer/vc-sdk/visdk41pubs/ApiReference/vim.DistributedVirtualSwitch.html
+"""
+
+from pyVmomi import vim
+from pycommon.pyVmomiService import *
+from pycommon.logger import getLogger
+
+_logger = getLogger("vCenterShell")
+
+
+class VirtualSwitchToMachineDisconnectCommand:
+    def __init__(self,
+                 pyvmomi_service,
+                 connection_retriever,
+                 synchronous_task_waiter):
+        self.pyvmomi_service = pyvmomi_service
+        self.connection_retriever = connection_retriever
+        self.synchronous_task_waiter = synchronous_task_waiter
+
+    def disconnect(self, vcenter_name, vm_uuid, network_name):
+        connection_details = self.connection_retriever.connection_details(vcenter_name)
+
+        si = self.pyvmomi_service.connect(connection_details.host, connection_details.username,
+                                          connection_details.password,
+                                          connection_details.port)
+        _logger.debug("Revoking ALL Interfaces from VM '{}'".format(vm_uuid))
+
+        vm = self.pyvmomi_service.find_by_uuid(si, vm_uuid)
+
+        return self.remove_interfaces_from_vm(vm, lambda device: self.is_device_match_network(device, network_name))
+
+    def disconnect_all(self, vcenter_name, vm_uuid):
+        connection_details = self.connection_retriever.connection_details(vcenter_name)
+
+        si = self.pyvmomi_service.connect(connection_details.host, connection_details.username,
+                                          connection_details.password,
+                                          connection_details.port)
+        _logger.debug("Revoking ALL Interfaces from VM '{}'".format(vm_uuid))
+
+        vm = self.pyvmomi_service.find_by_uuid(si, vm_uuid)
+        return self.remove_interfaces_from_vm(vm)
+
+    def is_device_match_network(self, device, network_name):
+        backing = device.backing
+
+        if hasattr(backing, 'network') and hasattr(backing.network, 'name'):
+            return network_name == backing.network.name
+        elif hasattr(backing, 'port') and hasattr(backing.port, 'portgroupKey'):
+            return network_name == backing.port.portgroupKey
+        return False
+
+    def remove_interfaces_from_vm(self, virtual_machine, filter_function=None):
+        """
+        @see https://www.vmware.com/support/developer/vc-sdk/visdk41pubs/ApiReference/vim.VirtualMachine.html#reconfigure
+        :param first_only:
+        :param filter_function: function that gets the device and decide if it should be deleted
+        :param virtual_machine: <vim.vm object>
+        :return:
+        """
+        device_change = []
+        for device in virtual_machine.config.hardware.device:
+            if isinstance(device, vim.vm.device.VirtualEthernetCard) and \
+                    (filter_function is None or filter_function(device)):
+                nicspec = vim.vm.device.VirtualDeviceSpec()
+                nicspec.operation = vim.vm.device.VirtualDeviceSpec.Operation.remove
+                nicspec.device = device
+                device_change.append(nicspec)
+
+        if len(device_change) > 0:
+            return self.remove_devices(device_change, virtual_machine)
+        return None
+
+    def remove_devices(self, device_change, virtual_machine):
+        config_spec = vim.vm.ConfigSpec(deviceChange=device_change)
+        task = virtual_machine.ReconfigVM_Task(config_spec)
+        logger.info("Virtual Machine remove ALL Interfaces task STARTED")
+        return self.synchronous_task_waiter.wait_for_task(task)
