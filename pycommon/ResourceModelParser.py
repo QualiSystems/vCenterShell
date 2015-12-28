0dc2a91c5c794c0d581f55d3f6a73a3fc2f6ea35
Boris Modylevsky <borismod@gmail.com>
2015-12-28 15:09:02 +0200

    ResourceModelParser parses resource attributes to attributes of ResourceModel classes
    connect #39

---
+++ pycommon/ResourceModelParser.py
@@ -0,0 +1,41 @@
+class ResourceModelParser:
+    def __init__(self):
+        pass
+
+    def parse_resource_model(self, resource_instance):
+        resource_class_name = get_resource_model_class_name(resource_instance.ResourceModelName)
+        instance = get_class('models.' + resource_class_name)
+        for prop in dir(instance):
+            if prop in resource_instance.attrib:
+                setattr(instance, prop, resource_instance.attrib[prop])
+        return instance
+
+
+def get_resource_model_class_name(resource_model):
+    """
+
+    :param resource_model:
+    :rtype: string
+    """
+    return resource_model.replace(' ', '') + 'ResourceModel'
+
+
+def get_class(class_path):
+    module_path, class_name = class_path.rsplit(".", 1)
+
+    try:
+        module = __import__(module_path, fromlist=[class_name])
+    except ImportError:
+        raise ValueError("Module '%s' could not be imported" % (module_path,))
+
+    try:
+        cls = getattr(module, class_name)
+    except AttributeError:
+        raise ValueError("Module '%s' has no class '%s'" % (module_path, class_name,))
+
+    try:
+        instance = getattr(cls, class_name)()
+    except TypeError as type_error:
+        raise ValueError('Failed to instantiate class {0}. Error: {1}'.format(class_name, type_error.message))
+
+    return instance
--- tests/test_DataModel.py
+++ tests/test_DataModel.py
@@ -3,6 +3,8 @@ import sys
 from os import listdir
 from os.path import join
 from unittest import TestCase
+
+from pycommon.ResourceModelParser import ResourceModelParser
 from pycommon.logging_service import LoggingService
 import xml.etree.ElementTree as ET
 
@@ -22,10 +24,10 @@ class TestDataModel(TestCase):
         self.assertGreater(len(resource_models), 0)
         validation_errors = []
         for resource_model in resource_models:
-            model_name = resource_model.attrib['Name'].replace(' ', '') + 'ResourceModel'
+            model_name = self.get_resource_model_class_name(resource_model.attrib['Name'])
 
             try:
-                klass = self.get_class('models.' + model_name)
+                klass = ResourceModelParser.get_class('models.' + model_name)
             except ValueError as value_error:
                 validation_errors.append(value_error.message)
                 continue
@@ -90,27 +92,9 @@ class TestDataModel(TestCase):
         return attribute_names
 
     def get_class_name_from_model_node(self, model_node):
-        return model_node.attrib['Name'].replace(' ', '') + 'ResourceModel'
+        resource_model = model_node.attrib['Name']
+        return ResourceModelParser.get_resource_model_class_name(resource_model)
 
     def get_attribute_name_from_attribute_node(self, attribute_node):
         return attribute_node.attrib['Name'].lower().replace(' ', '_')
 
-    def get_class(self, class_path):
-        module_path, class_name = class_path.rsplit(".", 1)
-
-        try:
-            module = __import__(module_path, fromlist=[class_name])
-        except ImportError:
-            raise ValueError("Module '%s' could not be imported" % (module_path,))
-
-        try:
-            cls = getattr(module, class_name)
-        except AttributeError:
-            raise ValueError("Module '%s' has no class '%s'" % (module_path, class_name,))
-
-        try:
-            instance = getattr(cls, class_name)()
-        except TypeError as type_error:
-            raise ValueError('Failed to instantiate class {0}. Error: {1}'.format(class_name, type_error.message))
-
-        return instance
