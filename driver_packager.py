fde5cb60ad435ed79a3040d9ae04e7d661cf8c9c
Alex A <aazarh@gmail.com>
2016-01-06 19:23:42 +0200

    add shell_packager and did a small refactoring to the driver_packager

--- driver_packager.py
+++ driver_packager.py
@@ -5,19 +5,24 @@ import zipfile
 DRIVER_FILE_NAME_FORMAT = 'vCenterShellPackage/Resource Scripts/{0}.zip'
 STRIPING_CHARS = ' \t\n\r'
 INCLUDE_DIRS = 'include_dirs'
+VERSION_FILENAME = 'version.txt'
 
 
-def zip_dir(path, ziph):
+def zip_dir(path, zip_handler, include_dir=True):
     """
     zip all files and items in dir
     :param path:
-    :param ziph:
+    :param zip_handler: zip file handler
+    :param boolean include_dir: specify if we want the archive with or without the directory
     """
     for root, dirs, files in os.walk(path):
         for file_to_zip in files:
             filename = os.path.join(root, file_to_zip)
             if os.path.isfile(filename):  # regular files only
-                ziph.write(filename)
+                if include_dir:
+                    zip_handler.write(filename)
+                else:
+                    zip_handler.write(filename, filename.split('\\', 1)[1])
 
 
 def ensure_dir(f):
@@ -26,47 +31,58 @@ def ensure_dir(f):
         os.makedirs(d)
 
 
-args = sys.argv
-confing_file_name = args[1]
-driver = args[2]
-target_name = args[3]
-
-print 'going to pack: {0}'.format(driver)
-
-with open('version.txt') as f_version:
-    if f_version is None:
+def add_version_file_to_zip(ziph):
+    if not os.path.exists(VERSION_FILENAME):
         raise Exception('no version file found')
-    version = f_version.read()
-    if not version:
-        raise Exception('no version file found')
-
-with open(confing_file_name) as f_config:
-    if f_config is None:
-        raise Exception('no packager config file found')
-    confing = dict()
-    config_raw = f_config.read().splitlines()
-    for att in config_raw:
-        cnf_att = att.split(':')
-        confing[cnf_att[0].strip(' \t\n\r')] = cnf_att[1].strip(STRIPING_CHARS).split(',')
+    ziph.write(VERSION_FILENAME)
 
 
-zip_name = DRIVER_FILE_NAME_FORMAT.format(target_name)
+def main(args):
+    config_file_name = args[1]
+    driver = args[2]
+    target_name = args[3]
 
-ensure_dir(zip_name)
+    print 'packing driver: {0}'.format(target_name)
 
-# deletes old package
-if os.path.isfile(zip_name):
-    os.remove(zip_name)
+    with open('version.txt') as f_version:
+        if f_version is None:
+            raise Exception('no version file found')
+        version = f_version.read()
+        if not version:
+            raise Exception('no version file found')
 
-zip_file = zipfile.ZipFile(zip_name, 'w')
+    with open(config_file_name) as f_config:
+        if f_config is None:
+            raise Exception('no packager config file found')
+        confing = dict()
+        config_raw = f_config.read().splitlines()
+        for att in config_raw:
+            cnf_att = att.split(':')
+            confing[cnf_att[0].strip(' \t\n\r')] = cnf_att[1].strip(STRIPING_CHARS).split(',')
 
-os.chdir(os.path.join(os.getcwd(), driver))
-zip_dir('.', zip_file)
-os.chdir(os.path.join(os.getcwd(), '../'))
+    zip_name = DRIVER_FILE_NAME_FORMAT.format(target_name)
 
-for dir_to_include in confing[INCLUDE_DIRS]:
-    zip_dir(dir_to_include, zip_file)
+    ensure_dir(zip_name)
 
-zip_file.close()
+    # deletes old package
+    if os.path.isfile(zip_name):
+        os.remove(zip_name)
 
-print 'done!'
+    zip_file = zipfile.ZipFile(zip_name, 'w')
+
+    os.chdir(os.path.join(os.getcwd(), driver))
+    zip_dir('.', zip_file)
+    os.chdir(os.path.join(os.getcwd(), '../'))
+
+    add_version_file_to_zip(zip_file)
+
+    for dir_to_include in confing[INCLUDE_DIRS]:
+        zip_dir(dir_to_include, zip_file)
+
+    zip_file.close()
+
+    print 'done!'
+
+
+if __name__ == "__main__":
+    main(sys.argv)
