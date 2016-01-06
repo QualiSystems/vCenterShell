d5f6112b702ac32c78e6ad7ad10b27b2e2d4e9f0
Alex A <aazarh@gmail.com>
2016-01-06 16:22:15 +0200

    fixed package building

--- driver_packager.py
+++ driver_packager.py
@@ -2,7 +2,7 @@ import os
 import sys
 import zipfile
 
-DRIVER_FILE_NAME_FORMAT = '../output/{0}_{1}.zip'
+DRIVER_FILE_NAME_FORMAT = 'vCenterShellPackage/Resource Scripts/{0}.zip'
 STRIPING_CHARS = ' \t\n\r'
 INCLUDE_DIRS = 'include_dirs'
 
@@ -27,10 +27,11 @@ def ensure_dir(f):
 
 
 args = sys.argv
-drivers = args[2:]
 confing_file_name = args[1]
+driver = args[2]
+target_name = args[3]
 
-print 'going to pack: {0}'.format(','.join(drivers))
+print 'going to pack: {0}'.format(driver)
 
 with open('version.txt') as f_version:
     if f_version is None:
@@ -49,24 +50,23 @@ with open(confing_file_name) as f_config:
         confing[cnf_att[0].strip(' \t\n\r')] = cnf_att[1].strip(STRIPING_CHARS).split(',')
 
 
-for driver in drivers:
-    zip_name = DRIVER_FILE_NAME_FORMAT.format(driver, version)
+zip_name = DRIVER_FILE_NAME_FORMAT.format(target_name)
 
-    ensure_dir(zip_name)
+ensure_dir(zip_name)
 
-    # deletes old package
-    if os.path.isfile(zip_name):
-        os.remove(zip_name)
+# deletes old package
+if os.path.isfile(zip_name):
+    os.remove(zip_name)
 
-    zip_file = zipfile.ZipFile(zip_name, 'w')
+zip_file = zipfile.ZipFile(zip_name, 'w')
 
-    os.chdir(os.path.join(os.getcwd(), driver))
-    zip_dir('.', zip_file)
-    os.chdir(os.path.join(os.getcwd(), '../'))
+os.chdir(os.path.join(os.getcwd(), driver))
+zip_dir('.', zip_file)
+os.chdir(os.path.join(os.getcwd(), '../'))
 
-    for dir_to_include in confing[INCLUDE_DIRS]:
-        zip_dir(dir_to_include, zip_file)
+for dir_to_include in confing[INCLUDE_DIRS]:
+    zip_dir(dir_to_include, zip_file)
 
-    zip_file.close()
+zip_file.close()
 
 print 'done!'
--- run_packager.bat
+++ run_packager.bat
@@ -1,3 +1,6 @@
-python driver_packager.py packeger_configs\\packeger_config_orchestration_service.txt orchestrationDriver
-python driver_packager.py packeger_configs\\packager_config_deployment_service_driver.txt deploymentDriver
-python driver_packager.py packeger_configs\\packager_config_vCenterShell.txt vCenterShellDriver
+python driver_packager.py packeger_configs\\packager_config_deployment_service_driver.txt deployment_service Deploy
+python driver_packager.py packeger_configs\\packager_config_vCenterShell.txt vCenterShell "Deploy From Template"
+python driver_packager.py packeger_configs\\packager_config_vCenterShell.txt vCenterShell "Destroy VM"
+python driver_packager.py packeger_configs\\packager_config_vCenterShell.txt vCenterShell Connect
+copy orchestration_service\driver.py "vCenterShellPackage\\Resource Scripts\\Deploy App.py" /Y
+copy vlan_service\vlan_driver.py "vCenterShellPackage\\Resource Scripts\\Connect All.py" /Y
