480c17429ecc2a4761849a0e962a32a8d8f06534
Sergii Tretiak <tretyak@gmail.com>
2016-01-06 23:41:05 -0800

    minor fix

--- tests/test_network/vnic/test_network_vnic.py
+++ tests/test_network/vnic/test_network_vnic.py
@@ -34,6 +34,14 @@ class TestNetwork(TestCase):
         device.backing.network.name = network_name
         self.assertTrue(device_is_attached_to_network(device, network_name))
 
+        network = Mock(spec=vim.Network)
+        network.name = "xnet"
+        nicspec = Mock()
+
+        nicspec.device = device
+        res = vnic_attach_to_network_standard(nicspec, network)
+        self.assertEquals(res.device.backing.network.name, "xnet")
+
     def test_device_attahed_to_network_distributed(self):
         network_name = "PORT-GROUP"
         device = Mock()
@@ -43,6 +51,15 @@ class TestNetwork(TestCase):
         del device.backing.network
         device.backing.port.portgroupKey = network_name
         self.assertTrue(device_is_attached_to_network(device, network_name))
+
+        port_group = Mock(spec=vim.dvs.DistributedVirtualPortgroup)
+        port_group.key = "group_net"
+        port_group.config.distributedVirtualSwitch.uuid = "6686"
+        nicspec = Mock()
+
+        nicspec.device = device
+        res = vnic_attach_to_network_distributed(nicspec, port_group)
+        self.assertEquals(res.device.backing.port.portgroupKey, "6686")
 
 
     def test_xx(self):
@@ -64,3 +81,9 @@ class TestNetwork(TestCase):
         nicspec = vnic_set_connectivity_status(nicspec, connect_status)
         self.assertEquals(nicspec.device.connectable.connected, connect_status)
 
+
+    def test_vnic_is_attachet_to_network(self):
+        nicspec = Mock()
+        nicspec.device = Mock()
+        res = vnic_is_attachet_to_network(nicspec, Mock())
+        self.assertFalse(res)
