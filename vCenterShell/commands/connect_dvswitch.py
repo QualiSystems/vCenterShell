f6eab3629bb404f294b9e7bd92ce318507f6f078
Sergii Tretiak <tretyak@gmail.com>
2016-01-12 23:54:03 -0800

    Documenting

--- vCenterShell/commands/connect_dvswitch.py
+++ vCenterShell/commands/connect_dvswitch.py
@@ -1,10 +1,14 @@
 
 
 class VirtualSwitchConnectCommand:
-    def __init__(self, pv_service, virtual_switch_to_machine_connector,
-                 dv_port_group_name_generator, vlan_spec_factory, vlan_id_range_parser):
+    def __init__(self,
+                 pv_service,
+                 virtual_switch_to_machine_connector,
+                 dv_port_group_name_generator,
+                 vlan_spec_factory,
+                 vlan_id_range_parser):
         """
-        :type pv_service: object
+        :param py_service: vCenter API wrapper
         :param virtual_switch_to_machine_connector:
         :param dv_port_group_name_generator: DvPortGroupNameGenerator
         :param vlan_spec_factory: VlanSpecFactory
@@ -16,6 +20,14 @@ class VirtualSwitchConnectCommand:
         self.vlan_id_range_parser = vlan_id_range_parser
 
     def connect_to_networks(self, si, vm_uuid, vm_network_mappings, default_network):
+        """
+        Connect VM to Network
+        :param si: VmWare Service Instance - defined connection to vCenter
+        :param vm_uuid: <str> UUID for VM
+        :param vm_network_mappings: <collection of 'VmNetworkMapping'>
+        :param default_network: <Network obj>
+        :return: None
+        """
         vm = self.pv_service.find_by_uuid(si, vm_uuid)
         default_network_instance = self.pv_service.get_network_by_full_name(si, default_network)
 
