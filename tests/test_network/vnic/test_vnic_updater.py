f7c3baaa21f30e90081ead29c884af69366733fe
Boris Modylevsky <borismod@gmail.com>
2016-01-13 11:15:50 +0200

    test_vnic_updater was added
    connect #40

---
+++ tests/test_network/vnic/test_vnic_updater.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+from mock import Mock
+from vCenterShell.network.vnic.vnic_updater import VnicUpdater
+from pyVmomi import vim
+
+
+class TestVnicUpdater(TestCase):
+    def test_update_vnics(self):
+
+        # Arrange
+        helpers = Mock()
+        vnic_updater = VnicUpdater(helpers)
+
+        # Act
+        vnic_updater.update_vnics([(vim.vm.device.VirtualEthernetCard(), Mock(), Mock())], "VM1", "VLAN1")
+
+        # Assert
+        self.assertTrue(helpers.SetConnectorAttributes.called)
