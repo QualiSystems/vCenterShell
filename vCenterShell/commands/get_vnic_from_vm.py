0af248d3c8fbb6043a01c995aeed1de3c884bbf9
Alex A <aazarh@gmail.com>
2016-01-17 11:49:36 +0200

    fixed failing tests

--- vCenterShell/commands/get_vnic_from_vm.py
+++ vCenterShell/commands/get_vnic_from_vm.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-import qualipy.scripts.cloudshell_scripts_helpers as helpers
 from pyVmomi import vim
 from models.VirtualNicModel import VirtualNicModel
 from common.logger import getLogger
@@ -14,28 +13,23 @@ class ConnectionException(Exception):
 
 
 class NetworkAdaptersRetrieverCommand(object):
-    def __init__(self, pv_service, cs_retriever_service, resource_connection_details_retriever):
+    def __init__(self, pv_service):
         """
         :param pv_service: <common.pv_service obj>
-        :param cs_retriever_service: <common.CloudshellDataRetrieverService obj>
-        :param resource_connection_details_retriever: <common.ResourceConnectionDetailsRetriever obj>
         :return:
         """
-        self.pvService = pv_service
-        self.csRetrieverService = cs_retriever_service
-        self.resourceConnectionDetailsRetriever = resource_connection_details_retriever
+        self.pv_service = pv_service
 
     def retrieve(self, si, path, network_name):
         """
         Retrieve Network by Name
-        :param pv_service: <common.pv_service obj>
         :param si: <service instance>
-        :param path: <str>
+        :param path: <str> the path to find the vm network
         :param network_name: <str>
         :return: <list of 'VirtualNicModel'>
         """
         _logger.debug("Retrieving Network... Path: '{0}' Name: '{1}'".format(path, network_name))
-        vm_machine = self.pvService.find_network_by_name(si, path, network_name)
+        vm_machine = self.pv_service.find_network_by_name(si, path, network_name)
 
         result = [VirtualNicModel(x.deviceInfo.summary,
                                   x.macAddress,
