8aa26acc0447677ed2eb9ab7533fd83b4bab760d
Sergii Shmarkatiuk <sergey.shmarkatyuk@gmail.com>
2016-01-21 18:18:00 +0200

    fixed failing test-case TestDvPortGroupCreator

--- tests/test_network/test_dvswitch/test_creator.py
+++ tests/test_network/test_dvswitch/test_creator.py
@@ -6,6 +6,7 @@ from vCenterShell.network.dvswitch.creator import DvPortGroupCreator
 
 
 class TestDvPortGroupCreator(TestCase):
+
     def test_create_dv_port_group_exception(self):
         # Arrange
         pyvmomy_service = Mock()
@@ -20,15 +21,17 @@ class TestDvPortGroupCreator(TestCase):
                           dv_port_group_creator.create_dv_port_group, 'port_name', 'switch_name', 'switch_path', Mock(),
                           None, None)
 
+
+
     def test_create_dv_port_group(self):
         # Arrange
-
         dv_switch = create_autospec(spec=vim.DistributedVirtualSwitch)
         dv_switch.portgroup = Mock()
         pyvmomy_service = Mock()
         pyvmomy_service.find_network_by_name = Mock(return_value=dv_switch)
         synchronous_task_waiter = Mock()
         dv_port_group_creator = DvPortGroupCreator(pyvmomy_service, synchronous_task_waiter)
+        dv_port_group_create_task_prev = DvPortGroupCreator.__dict__['dv_port_group_create_task']
         DvPortGroupCreator.dv_port_group_create_task = Mock()
 
         # Act
@@ -37,6 +40,7 @@ class TestDvPortGroupCreator(TestCase):
 
         # Assert
         self.assertTrue(dv_port_group_creator.dv_port_group_create_task.called)
+        setattr(DvPortGroupCreator, 'dv_port_group_create_task',dv_port_group_create_task_prev)
 
     def test_dv_port_group_create_task(self):
         #arrange
