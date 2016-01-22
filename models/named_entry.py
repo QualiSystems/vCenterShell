cc17956a398c0c17893014cdae3c77fd11e319bb
Sergii Shmarkatiuk <sergey.shmarkatyuk@gmail.com>
2016-01-22 15:43:48 +0200

    commented out NamedEntry and VirtualSwitch classes as long as they are not used

--- models/named_entry.py
+++ models/named_entry.py
@@ -1,18 +1,20 @@
 # -*- coding: utf-8 -*-
 
 from common.logger import getLogger
-_logger = getLogger("vCenterCommon")
 
-
-class NamedEntry(object):
-    """
-    Defines abstract named entry
-    """
-    def __init__(self, entry_name):
-        """
-        :param entry_name: <str> name of entry
-        """
-        self.name = entry_name
+### NOT USED:
+# _logger = getLogger("vCenterCommon")
+#
+#
+# class NamedEntry(object):
+#     """
+#     Defines abstract named entry
+#     """
+#     def __init__(self, entry_name):
+#         """
+#         :param entry_name: <str> name of entry
+#         """
+#         self.name = entry_name
 
 
 
