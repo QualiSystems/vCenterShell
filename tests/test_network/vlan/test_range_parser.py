e9580163edc0f6983793448b1228dba2b3c4ca9f
Sergii Shmarkatiuk <sergey.shmarkatyuk@gmail.com>
2016-01-22 16:05:28 +0200

    added tests for exception handling

--- tests/test_network/vlan/test_range_parser.py
+++ tests/test_network/vlan/test_range_parser.py
@@ -4,7 +4,7 @@ from vCenterShell.network.vlan.range_parser import VLanIdRangeParser
 
 
 class TestVLanIdRangeParser(TestCase):
-    def test_one_numer(self):
+    def test_one_number(self):
         v_lan_id_range_parser = VLanIdRangeParser()
         vlan_id = v_lan_id_range_parser.parse_vlan_id('Trunk', '11')
 
@@ -18,7 +18,7 @@ class TestVLanIdRangeParser(TestCase):
         self.assertEqual(vlan_id[0].start, 11)
         self.assertEqual(vlan_id[0].end, 100)
 
-    def test_two_numbers_with_dash(self):
+    def test_one_number_access_mode(self):
         v_lan_id_range_parser = VLanIdRangeParser()
         vlan_id = v_lan_id_range_parser.parse_vlan_id('Access', '100')
 
@@ -27,3 +27,15 @@ class TestVLanIdRangeParser(TestCase):
     def test_parse_vlan_id(self):
         v_lan_id_range_parser = VLanIdRangeParser()
         self.assertRaises(Exception, v_lan_id_range_parser.parse_vlan_id, '', None)
+
+    def test_not_a_number_vlan_id(self):
+        v_lan_id_range_parser = VLanIdRangeParser()
+        self.assertRaises(KeyError, v_lan_id_range_parser.parse_vlan_id, 'Access', 'a')
+
+    def test_three_vlan_id_parts(self):
+        v_lan_id_range_parser = VLanIdRangeParser()
+        self.assertRaises(Exception, v_lan_id_range_parser.parse_vlan_id, 'Trunk', '1-100-1000')
+
+    def test_unknown_mode(self):
+        v_lan_id_range_parser = VLanIdRangeParser()
+        self.assertRaises(KeyError, v_lan_id_range_parser.parse_vlan_id, 'Unknown', '100')
