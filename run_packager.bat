9fc8b53c5e2e7ebf343ede9766c9a8af3cd75396
Raz Abadi <razaba@gmail.com>
2016-02-01 16:43:36 +0200

    removing unnecessary commands on the vcenter model

--- run_packager.bat
+++ run_packager.bat
@@ -5,16 +5,8 @@ del /F /Q "vCenterShellPackage\Resource Scripts\."
 del /F /Q "vCenterShellPackage\Topology Scripts\."
 
 python driver_packager.py package_specs\\deployment_service_driver.ini
-python driver_packager.py package_specs\\deploy_from_template_command.ini
-python driver_packager.py package_specs\\destroy_vm_command.ini
-python driver_packager.py package_specs\\power_on_command.ini
-python driver_packager.py package_specs\\power_off_command.ini
-python driver_packager.py package_specs\\refresh_ip_command.ini
-python driver_packager.py package_specs\\connect_command.ini
-python driver_packager.py package_specs\\connect_bulk_command.ini
 python driver_packager.py package_specs\\vlan_auto_service.ini
 python driver_packager.py package_specs\\orchestration_service.ini
-
 python driver_packager.py package_specs\\deployed_app_proxy_connect.ini
 python driver_packager.py package_specs\\deployed_app_proxy_power_on.ini
 python driver_packager.py package_specs\\deployed_app_proxy_power_off.ini
