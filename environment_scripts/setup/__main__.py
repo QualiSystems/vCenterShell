dd58a1ba1c631a2d5f20d696f1a663716fd4deab
Raz Abadi <razaba@gmail.com>
2016-02-24 13:34:12 +0200

    setup script

--- environment_scripts/setup/__main__.py
+++ environment_scripts/setup/__main__.py
@@ -1,7 +1,10 @@
+import time
+
 from environment_scripts.setup.setup_script import execute_environment_setup
 
 
 def main():
+    time.sleep(30)
     execute_environment_setup()
 
 if __name__ == "__main__":
