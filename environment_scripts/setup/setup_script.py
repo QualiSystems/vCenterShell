09a798c97ef7a0d735e0aac1530fc9357aeb13d9
Alex A <aazarh@gmail.com>
2016-02-25 14:53:14 +0200

    before merge from develop

--- environment_scripts/setup/setup_script.py
+++ environment_scripts/setup/setup_script.py
@@ -1,60 +1,79 @@
+import threading
+
 import cloudshell.api.cloudshell_scripts_helpers as helpers
 from cloudshell.api.cloudshell_api import *
 
 from common.logger import getLogger
 
-logger = getLogger("App Orchestration Driver")
-
-api = helpers.get_api_session()
-reservation_id = helpers.get_reservation_context_details().id
-reservation_detailes = api.GetReservationDetails(reservation_id)
+logger = getLogger("CloudShell Sandbox Setup")
 
 
-def execute_environment_setup():
-    deploy_result = _deploy_apps_in_reservation()
+class EnvironmentSetup:
+    def __init__(self):
+        self.reservation_id = helpers.get_reservation_context_details().id
 
-    if deploy_result:
-        connect_result = _connect_all_routes_in_reservation()
+    def execute(self):
+        api = helpers.get_api_session()
+        reservation_details = api.GetReservationDetails(self.reservation_id)
 
-        power_on_result = _power_apps_in_reservation()
+        deploy_result = self._deploy_apps_in_reservation(api, reservation_details)
+        connect_result = self._connect_all_routes_in_reservation(api, reservation_details)
+        power_on_result = self._power_deployed_apps_and_refresh_ip(api, deploy_result)
 
+    def _power_deployed_apps_and_refresh_ip(self, api, deploy_result):
+        if not deploy_result.ResultItems:
+            logger.info("No deployed apps found in reservation {0}".format(self.reservation_id))
+            return None
 
-def _power_apps_in_reservation():
-    apps = reservation_detailes.ReservationDescription.Apps
-    if not apps:
-        logger.info("No apps found in reservation {0}".format(reservation_id))
-        return None
-
-    for app in apps:
-        return api.ExecuteResourceCommand(reservation_id, 'vCenert', 'PowerOn')
-
-
-def _deploy_apps_in_reservation():
-    apps = reservation_detailes.ReservationDescription.Apps
-    if not apps:
-        logger.info("No apps found in reservation {0}".format(reservation_id))
-        return None
-
-    app_names = map(lambda x: x.Name, apps)
-    app_inputs = map(lambda x: DeployAppInput(x.Name, "Name", x.Name), apps)
-
-    logger.info("deploying apps for reservation {0}, name:{1}".format(reservation_detailes, app_names))
-
-    result = api.ExecuteDeployAppCommandBulk(reservation_id, app_names, app_inputs)
-    return result
+        for resultItem in deploy_result.ResultItems:
+            if resultItem.Success:
+                pass
+            else:
+                logger.info("Failed to deploy app {0} in reservation {1}. Error: {2}."
+                            .format(resultItem.AppName, self.reservation_id, resultItem.Error))
 
 
-def _connect_all_routes_in_reservation():
-    connectors = [connector for connector in reservation_detailes.ReservationDescription.Connectors]
 
-    endpoints = []
-    for endpoint in connectors:
-        endpoints.append(endpoint.Target)
-        endpoints.append(endpoint.Source)
+    def _deploy_apps_in_reservation(self, api, reservation_details):
+        apps = reservation_details.ReservationDescription.Apps
+        if not apps:
+            logger.info("No apps found in reservation {0}".format(self.reservation_id))
+            return None
 
-    if len(endpoints) == 0:
-        logger.info("No routes to connect for reservation {0}".format(reservation_id))
-        return
+        app_names = map(lambda x: x.Name, apps)
+        app_inputs = map(lambda x: DeployAppInput(x.Name, "Name", x.Name), apps)
 
-    logger.info("Executing connect for reservation {0}".format(reservation_id))
-    return api.ConnectRoutesInReservation(reservation_id, endpoints, 'bi')
+        logger.info(
+                "Deploying apps for reservation {0}. App names: {1}".format(reservation_details, app_names.join(",")))
+
+        return api.ExecuteDeployAppCommandBulk(self.reservation_id, app_names, app_inputs)
+
+    def _connect_all_routes_in_reservation(self, api, reservation_details):
+        connectors = reservation_details.ReservationDescription.Connectors
+        endpoints = []
+        for endpoint in connectors:
+            endpoints.append(endpoint.Target)
+            endpoints.append(endpoint.Source)
+
+        if len(endpoints) == 0:
+            logger.info("No routes to connect for reservation {0}".format(self.reservation_id))
+            return
+
+        logger.info("Executing connect routes for reservation {0}".format(self.reservation_id))
+
+        return api.ConnectRoutesInReservation(self.reservation_id, endpoints, 'bi')
+
+
+class PowerOnAppThread(threading.Thread):
+    POWER_ON_COMMAND = "PowerOn"
+    REFRESH_IP_COMMAND = "Refresh IP"
+
+    def __init__(self, api, reservation_id, app_name, deployed_app_name):
+        threading.Thread.__init__(self)
+        self.api = api
+        self.reservationId = reservation_id
+        self.app_name = app_name
+        self.deployed_app_name = deployed_app_name
+
+    def run(self):
+        api.ExecuteResourceConnectedCommand(self.reservationId, self.deployed_app_name, "")
