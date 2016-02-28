bca671e1c9da0b0dd957082500caa57b24195cb2
Boris Modylevsky <borismod@gmail.com>
2016-02-28 15:41:52 +0200

    refresh_ip was improved to support cancellation
    connect #152

--- vCenterShell/commands/refresh_ip.py
+++ vCenterShell/commands/refresh_ip.py
@@ -2,6 +2,7 @@ import re
 import time
 
 from common.logger import getLogger
+from vCenterShell.commands.ip_result import IpResult, IpReason
 
 logger = getLogger(__name__)
 
@@ -14,7 +15,7 @@ class RefreshIpCommand(object):
         self.pyvmomi_service = pyvmomi_service
         self.resource_model_parser = resource_model_parser
 
-    def refresh_ip(self, si, session, vm_uuid, resource_name, default_network):
+    def refresh_ip(self, si, session, vm_uuid, resource_name, default_network, cancellation_context):
         """
         Refreshes IP address of virtual machine and updates Address property on the resource
 
@@ -23,26 +24,28 @@ class RefreshIpCommand(object):
         :param str vm_uuid: UUID of Virtual Machine
         :param str resource_name: Logical resource name to update address property on
         :param vim.Network default_network: the default network
+        :param cancellation_context:
         """
         match_function = self._get_ip_match_function(session, resource_name)
 
         vm = self.pyvmomi_service.find_by_uuid(si, vm_uuid)
 
         if vm.guest.toolsStatus == 'toolsNotInstalled':
-            raise ValueError('VMWare Tools status on VM {0} are not installed'.format(resource_name))
+            raise ValueError('VMWare Tools status on virtual machine \'{0}\' are not installed'.format(resource_name))
 
-        ip = self._obtain_ip(vm, default_network, match_function)
+        ip_result = self._obtain_ip(vm, default_network, match_function, cancellation_context)
 
-        if ip is None:
-            raise ValueError('IP address of VM {0} could not be obtained during {1} seconds'
+        if ip_result.reason == IpReason.Timeout:
+            raise ValueError('IP address of VM \'{0}\' could not be obtained during {1} seconds'
                              .format(resource_name, self.TIMEOUT))
 
-        session.UpdateResourceAddress(resource_name, ip)
+        if ip_result.reason == IpReason.Success:
+            session.UpdateResourceAddress(resource_name, ip_result.ip_address)
 
     @staticmethod
     def _get_ip_match_function(session, resource_name):
 
-        logger.debug('Trying to obtain IP address for {0}'.format(resource_name))
+        logger.debug('Trying to obtain IP address for \'{0}\''.format(resource_name))
 
         resource = session.GetResourceDetails(resource_name)
         ip_regexes = []
@@ -58,31 +61,31 @@ class RefreshIpCommand(object):
 
         if ip_regexes:
             ip_regex = ip_regexes[0]
-            logger.debug('Custom IP Regex to filter IP addresses {0}'.format(ip_regex))
+            logger.debug('Custom IP Regex to filter IP addresses \'{0}\''.format(ip_regex))
 
         return re.compile(ip_regex).match
 
-    def _obtain_ip(self, vm, default_network, match_function):
+    def _obtain_ip(self, vm, default_network, match_function, cancellation_context):
         time_elapsed = 0
         ip = None
         interval = self.TIMEOUT / 100
         while time_elapsed < self.TIMEOUT and ip is None:
+            if cancellation_context.is_cancelled:
+                return IpResult(ip, IpReason.Cancelled)
             ips = RefreshIpCommand._get_ip_addresses(vm, default_network)
             if ips:
-                print 'filtering ip by v4'
-                logger.debug('Filtering IP adresses to limit to IP V4 {0}'.format(','.join(ips)))
+                logger.debug('Filtering IP adresses to limit to IP V4 \'{0}\''.format(','.join(ips)))
                 ips = RefreshIpCommand._select_ip_by_match(ips, RefreshIpCommand.IP_V4_PATTERN.match)
             if ips:
-                print 'filtering ip by custom filter'
                 logger.debug('Filtering IP adresses by custom IP Regex'.format(','.join(ips)))
                 ips = RefreshIpCommand._select_ip_by_match(ips, match_function)
             if ips:
                 ip = ips[0]
                 if ip:
-                    return ip
+                    return IpResult(ip, IpReason.Success)
                 time_elapsed += interval
                 time.sleep(interval)
-        return ip
+        return IpResult(ip, IpReason.Timeout)
 
     @staticmethod
     def _get_ip_addresses(vm, default_network):
