51b20d6f30e1d4515e89d84ad99576bf4473d3ca
Raz Abadi <razaba@gmail.com>
2016-03-17 15:02:29 +0200

    fixing test and adding test_power_management_vm

--- tests/test_commands/test_power_management_vm.py
+++ tests/test_commands/test_power_management_vm.py
@@ -36,7 +36,7 @@ class TestVirtualMachinePowerManagementCommand(TestCase):
         self.assertTrue(synchronous_task_waiter.wait_for_task.called_with(task))
         self.assertTrue(vm.PowerOn.called)
 
-    def test_power_off(self):
+    def test_power_off_soft(self):
         # arrange
         vcenter_name = 'vcenter name'
         vm_uuid = 'uuid'
@@ -56,12 +56,46 @@ class TestVirtualMachinePowerManagementCommand(TestCase):
         power_manager._connect_to_vcenter = Mock(return_value=si)
         power_manager._get_vm = Mock(return_value=vm)
 
+        vcenter = Mock()
+        vcenter.shutdown_method = 'soft'
         # act
-        res = power_manager.power_off(session, si, vm_uuid, None)
+        res = power_manager.power_off(session, si, vcenter, vm_uuid, None)
 
         # assert
         self.assertTrue(res)
+        self.assertTrue(vm.PowerOff.called)
         self.assertTrue(power_manager._connect_to_vcenter.called_with(vcenter_name))
         self.assertTrue(power_manager._get_vm.called_with(si, vm_uuid))
         self.assertTrue(synchronous_task_waiter.wait_for_task.called_with(task))
-        self.assertTrue(vm.PowerOff.called)
+
+    def test_power_off_hard(self):
+        # arrange
+        vcenter_name = 'vcenter name'
+        vm_uuid = 'uuid'
+        session = Mock()
+        si = Mock(spec=vim.ServiceInstance)
+        vm = Mock(spec=vim.VirtualMachine)
+        task = Mock()
+        pv_service = Mock()
+        pv_service.find_by_uuid = Mock(return_value=vm)
+
+        vm.PowerOff = Mock(return_value=task)
+
+        synchronous_task_waiter = Mock()
+        synchronous_task_waiter.wait_for_task = Mock(return_value=True)
+
+        power_manager = VirtualMachinePowerManagementCommand(pv_service, synchronous_task_waiter)
+        power_manager._connect_to_vcenter = Mock(return_value=si)
+        power_manager._get_vm = Mock(return_value=vm)
+
+        vcenter = Mock()
+        vcenter.shutdown_method = 'hard'
+        # act
+        res = power_manager.power_off(session, si, vcenter,vm_uuid, None)
+
+        # assert
+        self.assertTrue(res)
+        self.assertTrue(vm.ShutdownGuest.called)
+        self.assertTrue(power_manager._connect_to_vcenter.called_with(vcenter_name))
+        self.assertTrue(power_manager._get_vm.called_with(si, vm_uuid))
+        self.assertTrue(synchronous_task_waiter.wait_for_task.called_with(task))
