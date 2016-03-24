67c7f682a80b1c9f491bee217c87c53c9f1162a5
Roni Dover <roni.dover@gmail.com>
2016-03-25 00:05:13 +0200

    updating style

--- package/docs/conf.py
+++ package/docs/conf.py
@@ -104,14 +104,14 @@ pygments_style = 'sphinx'
 #keep_warnings = False
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
-todo_include_todos = True
+todo_include_todos = False
 
 
 # -- Options for HTML output ----------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+#html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
