d265f9d7b3fbeeb6bc8190b0e0b6d81285b8f5fe
Alex A <aazarh@gmail.com>
2016-03-29 16:42:58 +0300

    fix setup & orchestrator

--- package/setup.py
+++ package/setup.py
@@ -7,26 +7,25 @@ with open(os.path.join('version.txt')) as version_file:
 with open('requirements.txt') as f_required:
     required = f_required.read().splitlines()
 
-
 setup(
-    name="cloudshell-cp-vcenter",
-    author="Quali",
-    author_email="support@qualisystems.com",
-    description=("This shell enables setting up vCenter as a cloud provider in"
-                 "CloudShell. It supports connectivity, deployment and management operations"
-                 "used for Cloudshel sanboxes."),
-    packages=find_packages(),
-    test_suite='nose.collector',
-    test_requires=['Nose'],
-    package_data={'': ['*.txt']},
-    install_requires=required,
-    version=version_from_file,
-    include_package_data=True,
-    keywords = "sandbox cloud virtualization vcenter cmp cloudshell",
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Topic :: Software Development :: Libraries",
-        "License :: OSI Approved :: Apache Software License",
-    ]
+        name="cloudshell-cp-vcenter",
+        author="Quali",
+        author_email="support@qualisystems.com",
+        description=("This shell enables setting up vCenter as a cloud provider in"
+                     "CloudShell. It supports connectivity, deployment and management operations"
+                     "used for Cloudshel sanboxes."),
+        packages=find_packages(),
+        test_suite='nose.collector',
+        test_requires=['Nose'],
+        package_data={'': ['*.txt']},
+        install_requires=required,
+        version=version_from_file,
+        include_package_data=True,
+        keywords="sandbox cloud virtualization vcenter cmp cloudshell",
+        classifiers=[
+            "Development Status :: 4 - Beta",
+            "Topic :: Software Development :: Libraries",
+            "License :: OSI Approved :: Apache Software License",
+        ]
 
 )
