f612173e2f37386eea38abf497ead49020c73781
Roni Dover <roni.dover@gmail.com>
2016-04-03 11:20:02 +0300

    updating setup

--- package/setup.py
+++ package/setup.py
@@ -7,23 +7,28 @@ with open(os.path.join('version.txt')) as version_file:
 with open('requirements.txt') as f_required:
     required = f_required.read().splitlines()
 
+with open('test_requirements.txt') as f_tests:
+    required_for_tests = f_tests.read().splitlines()
+
 setup(
         name="cloudshell-cp-vcenter",
+        url='https://github.com/QualiSystems/vCenterShell',
         author="Quali",
-        author_email="support@qualisystems.com",
-        description=("This shell enables setting up vCenter as a cloud provider in"
-                     "CloudShell. It supports connectivity, deployment and management operations"
-                     "used for Cloudshel sanboxes."),
+        author_email="support@quali.com",
+        description=("This Shell enables setting up vCenter as a cloud provider in CloudShell. "
+                     "It supports connectivity, and adds new deployment types for apps which can be used in "
+                     "CloudShell sandboxes."),
         packages=find_packages(),
         test_suite='nose.collector',
-        test_requires=['Nose'],
+        test_requires=required_for_tests,
         package_data={'': ['*.txt']},
         install_requires=required,
         version=version_from_file,
         include_package_data=True,
         keywords="sandbox cloud virtualization vcenter cmp cloudshell",
         classifiers=[
-            "Development Status :: 4 - Beta",
+            "Development Status :: 5 - Production/Stable",
+            "Programming Language :: Python :: 2.7",
             "Topic :: Software Development :: Libraries",
             "License :: OSI Approved :: Apache Software License",
         ]
