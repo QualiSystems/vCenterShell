9978cd5403327bdfc76e86d78b62a247018ce494
Raz Abadi <razaba@gmail.com>
2016-04-17 17:26:11 +0300

    adding deploy from linked snapshot deployment option

--- package/cloudshell/tests/test_commands/test_deploy_vm.py
+++ package/cloudshell/tests/test_commands/test_deploy_vm.py
@@ -37,7 +37,7 @@ class TestDeployFromTemplateCommand(unittest.TestCase):
         result = deploy_command.execute_deploy_from_template(
             si=si,
             logger=Mock(),
-            deployment_params= deploy_params,
+            deployment_params=deploy_params,
             resource_context=resource_context)
 
         # assert
@@ -67,3 +67,71 @@ class TestDeployFromTemplateCommand(unittest.TestCase):
         # assert
         self.assertTrue(result)
         self.assertTrue(deployer.deploy_from_image.called_with(si, deployment_params, connectivity))
+
+    def test_deploy_clone_execute(self):
+        # arrange
+        deployer = Mock()
+        si = Mock()
+        template_model = Mock()
+
+        deploy_res = dict()
+        deploy_res['vm_path'] = 'path'
+        deploy_res['vm_name'] = 'name'
+        deploy_res['uuid'] = 'uuid'
+
+        template_model.template_name = 'temp name'
+        template_model.vm_folder = 'temp folder'
+        deployer.deploy_from_template = Mock(return_value=deploy_res)
+
+        template_resource_model = vCenterVMFromTemplateResourceModel()
+
+        deploy_params = DeployFromTemplateDetails(template_resource_model, 'VM Deployment')
+
+        deploy_command = DeployCommand(deployer)
+
+        resource_context = Mock()
+
+        # act
+        result = deploy_command.execute_deploy_clone_from_vm(
+            si=si,
+            logger=Mock(),
+            deployment_params=deploy_params,
+            resource_context=resource_context)
+
+        # assert
+        self.assertTrue(result)
+        self.assertTrue(deployer.deploy_from_template.called_with(si, deploy_params, resource_context))
+
+    def test_deploy_snapshot_execute(self):
+        # arrange
+        deployer = Mock()
+        si = Mock()
+        template_model = Mock()
+
+        deploy_res = dict()
+        deploy_res['vm_path'] = 'path'
+        deploy_res['vm_name'] = 'name'
+        deploy_res['uuid'] = 'uuid'
+
+        template_model.template_name = 'temp name'
+        template_model.vm_folder = 'temp folder'
+        deployer.deploy_from_template = Mock(return_value=deploy_res)
+
+        template_resource_model = vCenterVMFromTemplateResourceModel()
+
+        deploy_params = DeployFromTemplateDetails(template_resource_model, 'VM Deployment')
+
+        deploy_command = DeployCommand(deployer)
+
+        resource_context = Mock()
+
+        # act
+        result = deploy_command.execute_deploy_from_linked_clone(
+            si=si,
+            logger=Mock(),
+            deployment_params=deploy_params,
+            resource_context=resource_context)
+
+        # assert
+        self.assertTrue(result)
+        self.assertTrue(deployer.deploy_from_template.called_with(si, deploy_params, resource_context))
