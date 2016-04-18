8d87f2dcbdb73bbb120d1abb896d0745e7551dc9
unknown <boris.m@QS-IL-OMERB.qualisystems.local>
2016-04-18 11:11:21 +0300

    logging_decorator was added

---
+++ vcentershell_driver/logging_decorator.py
@@ -0,0 +1,42 @@
+import functools
+import inspect
+from cloudshell.shell.core.context_based_logger import get_logger_for_driver
+
+CONTEXT = "context"
+
+
+class LoggingDecorator(object):
+    """
+    Decorates invocation of a method by initializing a logger and injecting it into logger property of context
+     argument
+    """
+    def __call__(self, func):
+
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            if CONTEXT in kwargs:
+                context = kwargs[CONTEXT]
+            else:
+                command_args = LoggingDecorator._get_command_args(func)
+                context = command_args[CONTEXT]
+
+            context.logger = get_logger_for_driver(context)
+            f_result = func(*args, **kwargs)
+
+            return f_result
+
+        return wrapper
+
+    @staticmethod
+    def _get_command_args(command):
+        """
+        Returns list of arguments of a function or method
+        :param command: Method to analyze
+        :return: list of arguments
+        """
+        command_args = inspect.getargspec(command)[0]
+        if command_args and command_args[0] == 'self':
+            command_args.pop(0)
+        return command_args
+
+
