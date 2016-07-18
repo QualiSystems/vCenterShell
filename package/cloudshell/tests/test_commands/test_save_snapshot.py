59e701b0e67e7bd8128f913a0d8f4069a225d1a5
Boris Modylevsky <borismod@gmail.com>
2016-07-18 15:03:47 +0300

    add unit test to SaveSnapshotCommand

--- package/cloudshell/tests/test_commands/test_save_snapshot.py
+++ package/cloudshell/tests/test_commands/test_save_snapshot.py
@@ -1,3 +1,4 @@
+from pyVmomi import vim
 from unittest import TestCase
 
 from mock import Mock
@@ -6,7 +7,7 @@ from cloudshell.cp.vcenter.commands.save_snapshot import SaveSnapshotCommand
 
 
 class TestSaveSnapshotCommand(TestCase):
-    def test_save_snapshot(self):
+    def test_save_snapshot_succeeds(self):
         vm = Mock()
 
         pyvmomi_service = Mock()
@@ -23,3 +24,31 @@ class TestSaveSnapshotCommand(TestCase):
 
         # Assert
         vm.CreateSnapshot.called_with('new_snapshot', 'Created by CloudShell vCenterShell', False, True)
+
+    def test_exception_raised_when_save_snapshot_fails_with_no_permission_exception(self):
+        vm = Mock()
+        vm.CreateSnapshot = Mock(side_effect=vim.fault.NoPermission())
+        pyvmomi_service = Mock()
+        pyvmomi_service.find_by_uuid = Mock(return_value=vm)
+
+        save_snapshot_command = SaveSnapshotCommand(pyvmomi_service, Mock())
+        si = Mock()
+        logger = Mock()
+
+        # Act + Assert
+        self.assertRaises(Exception, save_snapshot_command.save_snapshot, si, logger, 'machine1', 'new_snapshot')
+        logger.error.assert_called()
+
+    def test_exception_raised_when_save_snapshot_fails_with_general_exception(self):
+        vm = Mock()
+        vm.CreateSnapshot = Mock(side_effect=Exception())
+        pyvmomi_service = Mock()
+        pyvmomi_service.find_by_uuid = Mock(return_value=vm)
+
+        save_snapshot_command = SaveSnapshotCommand(pyvmomi_service, Mock())
+        si = Mock()
+        logger = Mock()
+
+        # Act + Assert
+        self.assertRaises(Exception, save_snapshot_command.save_snapshot, si, logger, 'machine1', 'new_snapshot')
+        logger.error.assert_called()
