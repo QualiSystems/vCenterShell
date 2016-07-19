b07871336a08b1ed9bb6284b0b3cde8483ab156b
Boris Modylevsky <borismod@gmail.com>
2016-07-19 12:22:47 +0300

    docstrings were added in SnapshotRetriever

--- package/cloudshell/cp/vcenter/common/vcenter/vm_snapshots.py
+++ package/cloudshell/cp/vcenter/common/vcenter/vm_snapshots.py
@@ -1,15 +1,33 @@
+from pyVmomi import vim
+
+
 class SnapshotRetriever:
     def __init__(self):
         pass
 
     @staticmethod
     def get_vm_snapshots(vm):
+        """
+        Returns dictinary of snapshots of a given virtual machine
+        Key is full path to snapshot, for example RootSnapshot/ChildSnapshot/LeadSnapshot
+        Value is an instance of snapshot
+        :param vm: an instance of virtual machine
+        :type vim.vm.VirtualMachine
+        :return:
+        """
         if not vm.snapshot:
             return {}
         return SnapshotRetriever._get_snapshots_recursively(vm.snapshot.rootSnapshotList, '')
 
     @staticmethod
     def _get_snapshots_recursively(snapshots, snapshot_location):
+        """
+        Recursively traverses child snapshots and returns dictinary of snapshots
+        :param snapshots: list of snapshots to examine
+        :param snapshot_location: current path of snapshots
+        :return: dictinary of snapshot path and snapshot instances
+        :rtype: dict
+        """
         snapshot_paths = {}
 
         if not snapshots:
