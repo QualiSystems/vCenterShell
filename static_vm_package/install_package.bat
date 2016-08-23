2204f90f78518297c40f7ce6892dfd3b20bd033e
GilGald <gil.2006@gmail.com>
2016-08-23 12:27:32 +0300

    Changing the name of the package

--- static_vm_package/install_package.bat
+++ static_vm_package/install_package.bat
@@ -1,3 +1,3 @@
 @echo off
 python -m pip install qpm --no-cache-dir --upgrade
-python -m qpm install --package_name VCenterAutoloadVMDriver
+python -m qpm install --package_name VCenterAutoloadStaticVMDriver
