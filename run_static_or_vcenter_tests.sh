c96ca4623f05800776bc06d1719e7909dd8f9577
GilGald <gil.2006@gmail.com>
2016-08-28 13:59:04 +0300

    Running static VM and vCenter test

--- run_static_or_vcenter_tests.sh
+++ run_static_or_vcenter_tests.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 if [ "${CLOUD_SHELL_SHELL_CORE}" -eq 1 ]
 then
+	echo "Running vCenter Tests"
     python runtests.py --with-coverage  --cover-package=package --exclude-dir=integration
 else
+	echo "Running static VM Tests"
     python runtestsStaticVM.py --with-coverage  --cover-package=package --exclude-dir=integration
 fi 
