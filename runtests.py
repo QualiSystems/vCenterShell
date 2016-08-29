a5e74cdff74b07b37103711aa23aa11becca4742
GilGald <gil.2006@gmail.com>
2016-08-29 13:35:27 +0300

    rt

--- runtests.py
+++ runtests.py
@@ -1,3 +1,4 @@
+import re
 import nose
 import nose.config
 import sys
@@ -5,5 +6,13 @@ from nose.plugins.manager import DefaultPluginManager
 c = nose.config.Config()
 c.plugins=DefaultPluginManager()
 c.srcDirs = ['package']
+
+
+# c.ignoreFiles.append('vm_autoload_driver')
+c.ignoreFiles.append(re.compile(r'^vm_autoload_driver\.py$'))
+c.ignoreFiles.append(re.compile(r'^test_get_inventory\.py$'))
+c.ignoreFiles.append(re.compile(r'^DeployAppOrchestrationDriver$'))
+
+
 if not nose.run(config=c):
     sys.exit(1)
